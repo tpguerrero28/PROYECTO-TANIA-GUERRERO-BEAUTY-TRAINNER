db880fa5fbe48ca9dcc19a93101b003d17b88af1
TANIA GUERRERO <tp.guerrerom@uea.edu.ec>
2026-02-21 16:01:37 -0500

    Actualización

--- app.py
+++ app.py
@@ -10,17 +10,12 @@ def inicio():
 # Ruta dinámica para clientes
 @app.route('/reserva/<cliente>')
 def reserva(cliente):
-    return f'Hola {cliente}, tu reserva en Tania Guerrero Beauty Trainer ha sido registrada exitosamente.'
+    return f'Bienvenida {cliente}, tu reserva en Tania Guerrero Beauty Trainer ha sido registrada exitosamente.'
 
 # Ruta dinámica para servicios
 @app.route('/servicio/<nombre>')
 def servicio(nombre):
     return f'Servicio seleccionado: {nombre}. Pronto nos contactaremos contigo.'
-
-# Ruta dinámica para cursos
-@app.route('/curso/<estudiante>')
-def curso(estudiante):
-    return f'Bienvenida {estudiante} al programa de formación Beauty Trainer.'
 
 if __name__ == '__main__':
     app.run(debug=True)
